ee5d7f67acbe16651ef859c781885509ac6abb69
omorimoto <morimotujanaiyo@gmail.com>
2019-06-14 14:52:43 +0900

    User管理2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
- 
+
   def edit
   end
 
@@ -16,5 +16,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email)
   end
-
 end
